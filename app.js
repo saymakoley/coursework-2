3ebb40a5360e8221591bdadf250cba158040b01f
saymakoley <97885143+saymakoley@users.noreply.github.com>
2023-01-30 23:46:18 +0000

    add public images for lessons

--- app.js
+++ app.js
@@ -6,15 +6,23 @@ const uri =
   "mongodb+srv://Sayma:lkC4hLUH46poTdId@web-coursework-2.vjqikya.mongodb.net/?retryWrites=true&w=majority";
 
 const connectToDB = async function () {
+  try {
     mongoDB = await MongoClient.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverApi: ServerApiVersion.v1,
-      });
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    });
+    console.log("Successfully connected to MongoDB Atlas");
+  } catch (error) {
+    console.log("Failed to connect to MongoDB Atlas: ", error);
+    throw error;
+  }
 };
 
 const getDB = function () {
-  
+  if (!mongoDB) {
+    console.log("Connection Error");
+  }
   return mongoDB.db("sayma-coursework");
 };
 
