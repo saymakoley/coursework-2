c414bd3062d90fc465180846d2d41e7291cec979
saymakoley <97885143+saymakoley@users.noreply.github.com>
2023-01-30 23:51:36 +0000

    request logger middleware and add cors headers

--- app.js
+++ app.js
@@ -3,17 +3,38 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
+// set up CORS header middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// request logger middleware
+app.use((req, res, next) => {
+  console.log({
+    method: req.method,
+    path: req.url,
+    status: res.statusCode,
+  });
+  next();
+});
+
 let mongoDB;
 
 const uri =
   "mongodb+srv://Sayma:lkC4hLUH46poTdId@web-coursework-2.vjqikya.mongodb.net/?retryWrites=true&w=majority";
 
 const connectToDB = async function () {
-    mongoDB = await MongoClient.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverApi: ServerApiVersion.v1,
-      });
+  mongoDB = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
 };
 
 const getDB = function () {
